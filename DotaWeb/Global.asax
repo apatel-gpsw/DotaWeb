58d9fb5b8a0185bc1f8451aa5de1fd9aad14c20d
Akash Patel <akashnpatel009@gmail.com>
2018-10-15 15:01:57 -0700

    Add project files.

---
+++ DotaWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotaWeb.MvcApplication" Language="C#" %>
---
+++ DotaWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DotaWeb.Startup))]
+namespace DotaWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
