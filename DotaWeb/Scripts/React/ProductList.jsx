7daa00c5874528ebc550c0406ddba4d67f9c7054
Akash Patel <akashnpatel009@gmail.com>
2018-10-31 22:16:09 -0700

    Ractified table

--- DotaWeb/Scripts/React/ProductList.jsx
+++ DotaWeb/Scripts/React/ProductList.jsx
@@ -2,7 +2,7 @@
 	constructor() {
 		super();
 		this.state = {
-			ProductData: []
+			MatchData: []
 		}
 	}
 
@@ -12,46 +12,17 @@
 			//	return <tr key={index}><td>{p.Name}</td><td>{p.PlayerName}</td><td>{p.Kill}</td><td>{p.Deaths}</td></tr>;
 			//}));
 			this.setState({
-				ProductData: response.data
+				MatchData: response.data
 			});
 		});
 	}
 
-	render() {
-		let rows = [];
-		let playersData = this.state.ProductData.Players;
-		console.log(playersData);
-		if (playersData !== undefined) {
-			console.log(Object.keys(playersData).length);
-			playersData.map((player, i) => {
-				let NetWorth = (player.Gold * this.state.ProductData.Duration / 1000) + "K";
-				let LhDn = player.Last_Hits + " / " + player.Denies;
-				let GpXp = player.Gold_Per_Min + " / " + player.Xp_Per_Min;
-				let Damage = player.Hero_Damage / 1000 + "K";
-				rows.push(
-				<tr key={i}>
-					<td>{player.Name}</td>
-					<td>{player.PlayerName}</td>
-					<td>{player.Kills}</td>
-					<td>{player.Deaths}</td>
-					<td>{player.Kills}</td>
-					<td>{player.Assists}</td>
-					<td>{NetWorth}</td>
-					<td>{LhDn}</td>
-					<td>{GpXp}</td>
-					<td>{Damage}</td>
-					<td>{player.Hero_Healing}</td>
-					<td>{player.Tower_Damage}</td>
-					<td>{player.Kills}</td>
-				</tr>);
-			});
-		}
-
+	BuildTable(rows) {
 		return (
 			<section>
-				<h1>Products List</h1>
+				<h1>Players Info</h1>
 				<div>
-					<table>
+					<table className="table">
 						<thead>
 							<tr>
 								<th>Hero</th>
@@ -70,18 +41,83 @@
 							</tr>
 						</thead>
 						<tbody>
-							{rows
-								//this.state.ProductData.Players.map((p, index) => {
-								//	return <tr key={index}><td>{p.Name}</td><td> {p.PlayerName}</td><td>{p.Kills}</td><td>{p.Deaths}</td></tr>;
-								//})
-							}
+							{rows.slice(0,5)}
+						</tbody>
+					</table>
+					<table className="table">
+						<thead>
+							<tr>
+								<th>Hero</th>
+								<th>Player</th>
+								<th>K</th>
+								<th>D</th>
+								<th>A</th>
+								<th>NET</th>
+								<th>LH/DN</th>
+								<th>GPM/XPM</th>
+								<th>DMG</th>
+								<th>HEAL</th>
+								<th>BLD</th>
+								<th>ITEMS</th>
+								<th>BACKPACK</th>
+							</tr>
+						</thead>
+						<tbody>
+							{rows.slice(5, 10)}
 						</tbody>
 					</table>
 				</div>
+			</section>);
+	}
 
+	BuilldRows(matchData, player, index) {
+		let teamClass = index > 4 ? "dire" : "radiant";
+		let NetWorth = Math.round(player.Gold * matchData.Duration / 1000) + "K";
+		let LhDn = player.Last_Hits + " / " + player.Denies;
+		let GpXp = player.Gold_Per_Min + " / " + player.Xp_Per_Min;
+		let Damage = Math.round(player.Hero_Damage / 1000) + "K";
+		return (
+			<tr key={index} className={teamClass}>
+				<td>
+					<img className="img-semi-circle" src={player.HeroImage} alt="HeroImage" />
+				</td>
+				<td>{player.PlayerName}</td>
+				<td>{player.Kills}</td>
+				<td>{player.Deaths}</td>
+				<td>{player.Assists}</td>
+				<td>{NetWorth}</td>
+				<td>{LhDn}</td>
+				<td>{GpXp}</td>
+				<td>{Damage}</td>
+				<td>{player.Hero_Healing}</td>
+				<td>{player.Tower_Damage}</td>
+				<td>
+				<img className="img-semi-circle" src={player.Item0Image} alt="Item0 Image" />
+				<img className="img-semi-circle" src={player.Item1Image} alt="Item1 Image" />
+				<img className="img-semi-circle" src={player.Item2Image} alt="Item2 Image" />
+				<img className="img-semi-circle" src={player.Item3Image} alt="Item3 Image" />
+				<img className="img-semi-circle" src={player.Item4Image} alt="Item4 Image" />
+				<img className="img-semi-circle" src={player.Item5Image} alt="Item5 Image" />
+				</td>
+				<td>
+					<img className="img-semi-circle" src={player.Backpack0Image} alt="Backpack0 Image" />
+					<img className="img-semi-circle" src={player.Backpack1Image} alt="Backpack1 Image" />
+					<img className="img-semi-circle" src={player.Backpack2Image} alt="Backpack2 Image" />	
+				</td>
+			</tr>);
+	}
 
-			</section>
-		)
+	render() {
+		let rows = [];
+		let matchData = this.state.MatchData;
+		let playersData = matchData.Players;
+		if (playersData !== undefined) {
+			playersData.map((player, i) => {
+				rows.push(this.BuilldRows(matchData, player, i));
+			});
+		}
+
+		return (this.BuildTable(rows))
 	}
 }
 
